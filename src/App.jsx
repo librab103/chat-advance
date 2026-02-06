91dbc5865559e154d6b270d7dd20f7e4a4109f73
Matt Battle <librab103@gmail.com>
2026-02-06 00:16:11 -0500

    Added a login page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import NoMatch from './Components/NoMatch';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Products from './Pages/Products';
+import Login from './Pages/Login'; 
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<NoMatch />} />
+        {/*-- <Route path="*" element={<NoMatch />} /> */}
       </Routes>
     </>
   );
